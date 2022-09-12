ea4272afdffb829bb19f48124cf229f7785d99f1
Nathan Johnston <najohnston4@crimson.ua.edu>
2022-09-12 00:46:12 -0500

    Add project files.

---
+++ wwwroot/js/darkmode.js
@@ -0,0 +1,20 @@
+﻿// Dark Mode SESSION Storage
+$("#body, #sidebar").toggleClass(sessionStorage.toggled);
+
+function darkLight() {
+    /*DARK CLASS*/
+    if (sessionStorage.toggled != "dark") {
+        $("#body").toggleClass("dark", true);
+        sessionStorage.toggled = "dark";
+    } else {
+        $("#body").toggleClass("dark", false);
+        sessionStorage.toggled = "";
+    }
+}
+
+/*Add 'checked' property to input if background == dark*/
+if ($("#body").hasClass("dark")) {
+    $("#checkBox").prop("checked", true);
+} else {
+    $("#checkBox").prop("checked", false);
+}
